f3bcf6643a3108bffaa1d0dd99eb61d86aa68e72
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-02-05 09:46:17 +0000

    font size added unformatted

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +35,11 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, modifier: Modifier=Modifier)
 {
     Text(//this is a text composable to show a text box
-        text=message
+        text=message,
+        fontSize = 100.sp /* this is the font size it uses the scalable pixel unit
+        to account for different screen resoloutions and user font sizes*/
+
+
     )
 
 }
