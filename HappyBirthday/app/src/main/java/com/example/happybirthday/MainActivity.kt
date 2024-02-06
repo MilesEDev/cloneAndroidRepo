9e14aba632183f4856a32ead8553966b293bed92
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-02-06 13:17:13 +0000

    added comment to row format

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -34,16 +35,21 @@ class MainActivity : ComponentActivity() {
 @Composable /*composable added to allow for composables like text to be added */
 fun GreetingText(message: String ,from: String,modifier: Modifier = Modifier){
 
-    Text( /* added composable text function */
-        text = message,
-        fontSize = 100.sp, /* sp here is scalable pixel means that it will work for different
-        screen resoloutions and user prefered font sizes*/
-        lineHeight = 116.sp, /* set to 116 so that it fills whole char size and leaves a bit
-        of white space as well between lines */
-    )
-    Text(
-        text=from
-    )
+    Row {/*the row function with trailing lambda syntax to organise text in a row*/
+        Text(
+            /* added composable text function */
+            text = message,
+            fontSize = 30.sp,
+            /* sp here is scalable pixel means that it will work for different
+                    screen resoloutions and user prefered font sizes*/
+            lineHeight = 116.sp,
+            /* set to 116 so that it fills whole char size and leaves a bit
+                    of white space as well between lines */
+        )
+        Text(
+            text = from
+        )
+    }
 
 
 }
