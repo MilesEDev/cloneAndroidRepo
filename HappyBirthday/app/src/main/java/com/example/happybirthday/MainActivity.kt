60798cfd261f7fa13d8fb1fd8d48efc87f17979e
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-02-06 20:36:16 +0000

    added background image to be previewed

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.happybirthday
 
+import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -82,11 +85,29 @@ fun GreetingText(message: String ,from: String,modifier: Modifier = Modifier){
 
 
 }
+@Composable
+fun GreetingImage(message: String, from: String,modifier: Modifier=Modifier)
+{
+    val image=painterResource(R.drawable.androidparty)/*this uses the image resource uploaded
+    earlier and stores it in a constant so that we can later use it*/
+    Image(
+        painter=image,
+        contentDescription = null/*this is set to null so that accessibility assistance does
+        not try and read out the background image*/
+
+    )
+
+
+
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Miles", from = "leeds beckett")
+        GreetingImage(message = "Happy Birthday Miles", from = "leeds beckett")
     }
 }
