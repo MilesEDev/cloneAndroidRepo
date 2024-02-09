d02def6ee18fcf7e4ede25c7998947c7fe9202df
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-02-09 09:49:51 +0000

    added string resource file to allow for transalation

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,8 +37,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Miles",
-                        from = "from beckett")
+                        message = getString(R.string.happy_birthday_text),
+                        from = getString(R.string.signature_text))
 
 
 
@@ -129,6 +130,7 @@ fun GreetingImage(message: String, from: String,modifier: Modifier=Modifier)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Miles", from = "from beckett")
+        GreetingImage(message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text))
     }
 }
