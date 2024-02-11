5f4fa1e2a55619dbd427e126a88ef82e910605eb
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-02-11 16:50:25 +0000

    set corners to different colours although incorrect colours at the moment

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.quadrants
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.quadrants.ui.theme.QuadrantsTheme
 
@@ -31,7 +38,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
+    Column(modifier=Modifier.fillMaxHeight()) {
 
+
+        Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Green))
+        {
+            Box(modifier = Modifier
+                .weight(1f)
+                .background(Color.Magenta).fillMaxWidth().fillMaxHeight())
+            {
+                Text(text = "header")
+            }
+            Box(modifier = Modifier
+                .weight(1f)
+                .background(Color.Red).fillMaxWidth().fillMaxHeight())
+            {
+                Text(text = "header")
+            }
+
+
+
+
+        }
+        Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Green)) {
+            Box(modifier = Modifier
+                .weight(1f)
+                .background(Color.Blue).fillMaxWidth().fillMaxHeight())
+            {
+                Text(text = "header")
+            }
+
+            Box(modifier = Modifier
+                .weight(1f)
+                .background(Color.LightGray).fillMaxWidth().fillMaxHeight())
+            {
+                Text(text = "header")
+            }
+        }
+    }
 }
 
 
