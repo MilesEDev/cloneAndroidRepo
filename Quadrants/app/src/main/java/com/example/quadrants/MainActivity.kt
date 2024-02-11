72862c73798cbc2863680e5569b4bed5911a4ea1
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-02-11 18:13:08 +0000

    single quadrant text align working

--- Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
+++ Quadrants/app/src/main/java/com/example/quadrants/MainActivity.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quadrants.ui.theme.QuadrantsTheme
+import kotlin.io.encoding.Base64
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,36 +50,78 @@ fun MainPage(modifier: Modifier = Modifier) {
     Column(modifier=Modifier.fillMaxHeight()) {
 
 
-        Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Green))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .background(Color.Green))
         {
-            Box(modifier = Modifier
+            Box(contentAlignment = Alignment.Center, modifier = Modifier
                 .weight(1f)
-                .background(Color.Magenta).fillMaxWidth().fillMaxHeight())
+                .background(Color.Green)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(16.dp))
+
             {
-                Text(text = "header")
+                Column() {
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                        Text(
+
+
+                            text = "Text composable",
+
+                            fontWeight = Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+                    }
+
+
+                    Text(
+
+                        text =
+                        "Displays text and follows the recommended Material Design guidelines.",
+                        textAlign = TextAlign.Justify
+                    )
+                }
+
+
+
+
             }
             Box(modifier = Modifier
                 .weight(1f)
-                .background(Color.Red).fillMaxWidth().fillMaxHeight())
+                .background(Color.Red)
+                .fillMaxWidth()
+                .fillMaxHeight())
             {
-                Text(text = "header")
+
+                    Text(text = "header")
+
             }
 
 
 
 
         }
-        Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Green)) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .background(Color.Green)) {
             Box(modifier = Modifier
                 .weight(1f)
-                .background(Color.Blue).fillMaxWidth().fillMaxHeight())
+                .background(Color.Blue)
+                .fillMaxWidth()
+                .fillMaxHeight())
             {
                 Text(text = "header")
             }
 
             Box(modifier = Modifier
                 .weight(1f)
-                .background(Color.LightGray).fillMaxWidth().fillMaxHeight())
+                .background(Color.LightGray)
+                .fillMaxWidth()
+                .fillMaxHeight())
             {
                 Text(text = "header")
             }
