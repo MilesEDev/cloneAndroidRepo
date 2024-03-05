59d82305a62ea4d81edfea311b2447dbfc749be5
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-03-05 17:53:35 +0000

    %1

--- Cupcake/app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
+++ Cupcake/app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
@@ -105,14 +105,15 @@ fun SelectOptionScreen(
             horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
             verticalAlignment = Alignment.Bottom
         ){
-            OutlinedButton(modifier = Modifier.weight(1f), onClick = onCancelButtonClicked) {
+            OutlinedButton(modifier = Modifier.weight(1f),
+                onClick = onCancelButtonClicked) {
                 Text(stringResource(R.string.cancel))
             }
             Button(
                 modifier = Modifier.weight(1f),
                 // the button is enabled when the user makes a selection
                 enabled = selectedValue.isNotEmpty(),
-                onClick = {}
+                onClick = onNextButtonClicked
             ) {
                 Text(stringResource(R.string.next))
             }
