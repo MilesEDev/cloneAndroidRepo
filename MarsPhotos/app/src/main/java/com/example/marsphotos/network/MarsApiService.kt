d9772a277bb81b270c3eb34e6e485886f659c0c7
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-03-08 08:59:42 +0000

    missed get import which caused bug now resolved

--- MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,6 +1,7 @@
 package com.example.marsphotos.network
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
