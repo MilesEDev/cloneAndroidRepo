df3a58a044062c32eccdd4508aa94fc71c1fca3e
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-03-09 13:53:54 +0000

    card composable created not yet previewed

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.affirmations.ui.theme.AffirmationsTheme
+import com.example.affirmations.model.Affirmation
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,4 +54,35 @@ fun AffirmationsPreview() {
     AffirmationsTheme {
         MainPage()
     }
+}
+@Composable
+fun AffirmationsApp()
+{
+
+}
+@Composable
+fun AffirmationCard(affirmation: Affirmation,modifier: Modifier = Modifier)
+{
+    Card(modifier = modifier)
+    {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceID),
+                contentDescription = stringResource(affirmation.stringResourceID),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+
+
+            )
+            Text(
+                text= LocalContext.current.getString(affirmation.stringResourceID),
+                modifier = Modifier.padding(15.dp),
+                style= MaterialTheme.typography.headlineSmall
+            )
+
+        }
+    }
+
 }
