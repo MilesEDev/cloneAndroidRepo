bacdc426a366efa71967c50558180de5b7b418bd
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-05-05 13:18:12 +0100

    marsphotocard composable added with asynch image

--- MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -15,23 +15,28 @@
  */
 package com.example.marsphotos.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.marsphotos.R
-import com.example.marsphotos.ui.theme.MarsPhotosTheme
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.marsphotos.R
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
@@ -79,7 +84,18 @@ fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
         Text(text = photos)
     }
 }
+@Composable
+fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
 
+    AsyncImage(
+        model = ImageRequest.Builder(context = LocalContext.current)
+            .data(photo.imgSrc)
+            .crossfade(true)
+            .build(),
+        contentDescription = stringResource(R.string.mars_photo),
+        modifier = Modifier.fillMaxWidth()
+    )
+}
 @Preview(showBackground = true)
 @Composable
 fun ResultScreenPreview() {
