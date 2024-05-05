55be98986813ce69ad838a31afbec07cc83aa179
MIlesstudentacc <m.englefield4873@student.leedsbeckett.ac.uk>
2024-05-05 14:35:52 +0100

    retry added extension finished

--- MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ MarsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -48,15 +49,16 @@ import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
-    marsUiState: MarsUiState,
+    marsUiState: MarsUiState, retryAction: () -> Unit,
     modifier: Modifier = Modifier
+
 ) {
     when (marsUiState) {
         is MarsUiState.Loading-> LoadingScreen(modifier=modifier.fillMaxSize())
         is MarsUiState.Success-> PhotosGridScreen(photos = marsUiState.photos,modifier=modifier.fillMaxSize()
 
         )
-        is MarsUiState.Error -> ErrorScreen(modifier=modifier.fillMaxSize())
+        is MarsUiState.Error -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
     }
 }
 @Composable
@@ -68,7 +70,7 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
     )
 }
 @Composable
-fun ErrorScreen(modifier: Modifier = Modifier) {
+fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
@@ -78,6 +80,9 @@ fun ErrorScreen(modifier: Modifier = Modifier) {
             painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
         )
         Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
     }
 }
 /**
